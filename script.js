b8f32c6ffce03ba1f4130f502813b1c66af3c78e
saikat-sudipta-shuva <saikatsudiptashuva@gmail.com>
2025-12-30 20:01:23 +0600

    Design Video Card & Display Dynamic Data

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function dispalyCategories(categories){
     const categoryContainer= document.getElementById("category-container")
     //loop operation on array of object
     for (let cat of categories ){
-        console.log(cat);
-  
+    //console.log(cat);
+
     //create Element
     const categoryDiv=document.createElement("div");
     categoryDiv.innerHTML=`
@@ -31,3 +31,52 @@ function dispalyCategories(categories){
 }
 loadCategories();
 
+//video GET
+
+function loadVideos(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then(res=>res.json())
+    .then(data=>{
+       // console.log(data.videos);
+       displayVideos(data.videos);
+    })
+}
+
+const displayVideos=(videos)=>{
+    //console.log(videos);
+    const videoContainer = document.getElementById('video-container');
+    videos.forEach(video=>{
+        //console.log(video)
+        const videoCard=document.createElement("div");
+        videoCard.innerHTML=`
+        <div class="card bg-base-100">
+  <figure class="relative">
+    <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" alt="Shoes" />
+    <span class="absolute bottom-2 right-2 text-sm rounded text-white bg-black px-2">3hrs 56 min ago</span>
+  </figure>
+  <div class=" flex gap-3 px-2 py-5">
+    <div class="profile">
+        <div class="avatar">
+  <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring-2 ring-offset-2">
+    <img src="${video.authors[0].profile_picture}" />
+  </div>
+</div>
+    </div>
+    <div class="intro">
+        <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+        <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name}
+          <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="">
+        </p>
+        <p class="text-sm text-gray-400 flex gap-1">${video.others.views}</p>
+    </div>
+  </div>
+  <div>
+  </div>
+</div> 
+        `
+        videoContainer.append(videoCard);
+    });
+};
+
+
+loadVideos();
